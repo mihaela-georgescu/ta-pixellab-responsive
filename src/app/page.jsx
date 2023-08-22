3565dd0dadf006a57c5f651fa248027578fbc615
Dragos <dragos.i@adoreme.com>
2023-08-22 20:04:49 +0300

    2004

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -12,14 +12,24 @@ export default function Home() {
 
         <div className="mt-4">
           <button className="button button-primary">Hello</button>
-          <button className="button button-primary button-primary-lg">
-            Hello
-          </button>
+          <button className="button button-primary button-lg">Hello</button>
 
           <a href="/" className="button button-primary">
             Hello
           </a>
-          <a className="button button-primary button-primary-lg">Hello</a>
+          <a className="button button-primary button-lg">Hello</a>
+        </div>
+
+        <div className="mt-4">
+          <button className="button button-primary-outline">Hello</button>
+          <button className="button button-primary-outline button-lg">
+            Hello
+          </button>
+
+          <a href="/" className="button button-primary-outline">
+            Hello
+          </a>
+          <a className="button button-primary-outline button-lg">Hello</a>
         </div>
       </main>
 
