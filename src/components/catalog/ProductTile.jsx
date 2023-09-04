87f475fc8d379118b8d010a3b7c1e7947d951125
Mihaela Georgescu <mihaela.georgescu50@gmail.com>
2023-09-04 21:43:47 +0300

    styling header, footer, product tile

--- src/components/catalog/ProductTile.jsx
+++ src/components/catalog/ProductTile.jsx
@@ -15,7 +15,7 @@ export default function ProductTile() {
 
         <h1 className="product-tile-title">
           <a href="/" title="Thread Affair Monochrome Suit">
-            Thread Affair Monochrome Suit
+            <span>Thread Affair</span> Monochrome <span>Suit</span>
           </a>
         </h1>
 
@@ -26,7 +26,13 @@ export default function ProductTile() {
         </h2>
       </header>
 
-      <section className="product-tile-content">{/* add pricing */}</section>
+      <section className="product-tile-content">
+        <span className="product-catalog-price">499.00</span>
+
+        <span className="product-price">
+          USD <span>399.00</span>
+        </span>
+      </section>
 
       <footer className="product-tile-footer">
         <button className="button button-primary">Add to Cart</button>
--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -6,146 +6,146 @@ export default function Footer() {
           <nav className="footer-nav">
             <ul>
               <li>
-                <a href="/" title="">
+                <a href="/" title="Information">
                   Information
                 </a>
               </li>
 
               <li>
-                <a href="/" title="">
-                  Information
+                <a href="/" title="Contact Us">
+                  Contact Us
                 </a>
               </li>
 
               <li>
-                <a href="/" title="">
-                  Information
+                <a href="/" title="Track my order">
+                  Track my order
                 </a>
               </li>
 
               <li>
-                <a href="/" title="">
-                  Information
+                <a href="/" title="Shipping">
+                  Shipping
                 </a>
               </li>
 
               <li>
-                <a href="/" title="">
-                  Information
+                <a href="/" title="Free Returns">
+                  Free Returns
                 </a>
               </li>
 
               <li>
-                <a href="/" title="">
-                  Information
-                </a>
-              </li>
-
-              <li>
-                <a href="/" title="">
-                  Information
+                <a href="/" title="My Account">
+                  My Account
                 </a>
               </li>
             </ul>
 
             <ul>
               <li>
-                <a href="/" title="">
-                  Information
+                <a href="/" title="Services">
+                  Services
                 </a>
               </li>
 
               <li>
-                <a href="/" title="">
-                  Information
+                <a href="/" title="Ship to store">
+                  Ship to store
                 </a>
               </li>
 
               <li>
-                <a href="/" title="">
-                  Information
+                <a href="/" title="Gift card">
+                  Gift card
                 </a>
               </li>
 
               <li>
-                <a href="/" title="">
-                  Information
-                </a>
-              </li>
-            </ul>
-
-            <ul>
-              <li>
-                <a href="/" title="">
-                  Information
+                <a href="/" title="Online only">
+                  Online only
                 </a>
               </li>
 
               <li>
-                <a href="/" title="">
-                  Information
+                <a href="/" title="Tips & tricks">
+                  Tips & tricks
                 </a>
               </li>
 
               <li>
-                <a href="/" title="">
-                  Information
-                </a>
-              </li>
-
-              <li>
-                <a href="/" title="">
-                  Information
-                </a>
-              </li>
-
-              <li>
-                <a href="/" title="">
-                  Information
+                <a href="/" title="Home">
+                  Home
                 </a>
               </li>
             </ul>
 
             <ul>
               <li>
-                <a href="/" title="">
-                  Information
+                <a href="/" title="Loyalty programs">
+                  Loyalty programs
                 </a>
               </li>
 
               <li>
-                <a href="/" title="">
-                  Information
+                <a href="/" title="Sign up to The Simons">
+                  Sign up to The Simons
                 </a>
               </li>
 
               <li>
-                <a href="/" title="">
-                  Information
+                <a href="/" title="My Account">
+                  My Account
                 </a>
               </li>
 
               <li>
-                <a href="/" title="">
-                  Information
+                <a href="/" title="Conditions">
+                  Conditions
                 </a>
               </li>
 
               <li>
-                <a href="/" title="">
-                  Information
+                <a href="/" title="FAQ">
+                  FAQ
+                </a>
+              </li>
+            </ul>
+
+            <ul>
+              <li>
+                <a href="/" title="Careers">
+                  Careers
                 </a>
               </li>
 
               <li>
-                <a href="/" title="">
-                  Information
+                <a href="/" title="About Us">
+                  About Us
                 </a>
               </li>
 
               <li>
-                <a href="/" title="">
-                  Information
+                <a href="/" title="Our Story">
+                  Our Story
+                </a>
+              </li>
+
+              <li>
+                <a href="/" title="Art & Culture">
+                  Art & Culture
+                </a>
+              </li>
+
+              <li>
+                <a href="/" title="Our Private Labels">
+                  Our Private Labels
+                </a>
+              </li>
+
+              <li>
+                <a href="/" title="Stores">
+                  Stores
                 </a>
               </li>
             </ul>
@@ -156,31 +156,37 @@ export default function Footer() {
               <span>Thread Affair</span> Milano
             </h1>
             <p>
-              Largo Augusto 3, Milano, 20122, Milano, IT Tel: 0039 02 760 003 66
-              office@thread-affair.com
+              Largo Augusto 3, Milano, 20122, Milano, IT br
+              <a href="tel:+00390276000366" title="Call Us">
+                Tel: 0039 02 760 003 66
+              </a>
+              <br />
+              <a href="mailto:office@thread-affair.com" title="Email Us">
+                office@thread-affair.com
+              </a>
             </p>
           </section>
 
           <section className="footer-connect">
             <ul>
               <li>
-                <a href="" title="Support">
-                  <i className="fa-solid"></i>
+                <a href="tel:+0741000000" title="Call us">
+                  <i className="fa-solid fa-square-phone"></i>
                   Support
                 </a>
               </li>
 
               <li>
-                <a href="" title="Support">
-                  <i className="fa-solid"></i>
-                  Support
+                <a href="/" title="Chat">
+                  <i className="fa-solid fa-comments"></i>
+                  Chat
                 </a>
               </li>
 
               <li>
-                <a href="" title="Support">
-                  <i className="fa-solid"></i>
-                  Support
+                <a href="mailto:office@thread-affair.com" title="Email us">
+                  <i className="fa-solid fa-envelope-open-text"></i>
+                  Email
                 </a>
               </li>
             </ul>
@@ -188,7 +194,21 @@ export default function Footer() {
         </div>
       </div>
 
-      <div className="footer-secondary">DO UR HOMEWORK</div>
+      <div className="footer-secondary">
+        <p>©2020 THREAD AFFAIR. All Rights Reserved.</p>
+
+        <p>
+          Designed by
+          <a
+            href="http://pixellab.ro"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="Pixellab.ro"
+          >
+            <img src="/images/pixellab_logo.jpg" alt="Pixellab - Logo" />
+          </a>
+        </p>
+      </div>
     </>
   );
 }
